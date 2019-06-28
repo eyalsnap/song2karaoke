b9ff3514da70598b1928fe0fbb823951c57f129a
Eyal <eyal.snappir@gmail.com>
2019-06-28 17:38:41 +0300

    increase the net

--- nets/mock_net.py
+++ nets/mock_net.py
@@ -9,13 +9,13 @@ class MockNet(nn.Module):
         member variables.
         """
         super(MockNet, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=2, out_channels=12, kernel_size=1, stride=1, padding=0)
-        # self.conv2 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
-        # self.conv3 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
-        # self.conv4 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
-        # self.conv5 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
-        # self.conv6 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
-        self.conv7 = nn.Conv1d(in_channels=12, out_channels=2, kernel_size=1, stride=1, padding=0)
+        self.conv1 = nn.Conv1d(in_channels=2, out_channels=112, kernel_size=1, stride=1, padding=0)
+        self.conv2 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
+        self.conv3 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
+        self.conv4 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
+        self.conv5 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
+        self.conv6 = nn.Conv1d(in_channels=112, out_channels=112, kernel_size=1, stride=1, padding=0)
+        self.conv7 = nn.Conv1d(in_channels=112, out_channels=2, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         """
@@ -24,11 +24,11 @@ class MockNet(nn.Module):
         well as arbitrary operators on Tensors.
         """
         x = F.relu(self.conv1(x))
-        # x = F.relu(self.conv2(x))
-        # x = F.relu(self.conv3(x))
-        # x = F.relu(self.conv4(x))
-        # x = F.relu(self.conv5(x))
-        # x = F.relu(self.conv6(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
         x = F.relu(self.conv7(x))
 
         y_pred = x
