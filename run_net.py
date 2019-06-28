5865eb0aed2c7667e494e44f025951ebfd07923f
Eyal <eyal.snappir@gmail.com>
2019-06-28 17:00:57 +0300

    fixing path to linux

--- run_net.py
+++ run_net.py
@@ -6,7 +6,7 @@ def main():
     dir_paths = [
         # r'C:\Users\Eyal\Desktop\eyal\python\data\sample_data\sample_data\_nopr_slman__am_ate_shoma_',
         # nitzan comment for testing
-        r'C:\Users\Eyal\Desktop\eyal\python\data\sample_data\sample_data\_aiti_loi__zz_btirof_'
+        r'data/sample_data/_aiti_loi__zz_btirof_'
     ]# * 10
 
     audio_dataloader = build_dataloader(dir_paths)
